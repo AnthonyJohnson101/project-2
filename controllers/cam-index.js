b685131cf3dead66d147379a55b3fceefa5637ad
Cameron Schlueter <camsc@MacBook-Air-3.local>
2023-03-09 21:19:51 -0500

    Added app.engine to make handlebars the view engine, added dummy server in package.json.

--- controllers/cam-index.js
+++ controllers/cam-index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const multer = require ('multer');
+const app = express();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
@@ -7,3 +9,24 @@ router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
+
+// configuration
+
+const storage = multer.diskStorage({
+    destination: function(require, file, cb) {
+        cb(null, 'uploads/');
+    },
+});
+
+const upload = multer({ storage: storage });
+
+//route for file upload
+
+app.post('/upload', upload.single('file'), function(req, res) {
+    res.send("Upload Successful");
+});
+
+//start server
+app.listen(3000, function() {
+    console.log('Server started on port 3000');
+});
