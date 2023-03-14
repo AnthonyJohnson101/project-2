584c644c1d3c45d1c9c6335d0519d60f1f6e4283
benjammo <118003873+benjammo@users.noreply.github.com>
2023-03-13 20:17:38 -0400

    work done on the API routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Recipe } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
@@ -43,8 +43,6 @@ router.post('/logout', (req, res) => {
   }
 });
 
-module.exports = router;
-
 
 // route to create a user
 // router.post('/postrecipe', async (req, res) => {
@@ -56,32 +54,49 @@ module.exports = router;
 
 // route to post recipe
 router.post('/postrecipe', async (req, res) => {
-    // grab form information
-    //bring it into database
+
+//create table row based on form submission
+    await Recipe.create(req.body);
 
     //log it:
-    console.info(`${req.method} request received`);
+    console.info(`${req.method} request received to post new recipe`);
 });
 
+
 // Route to get the user's recipes
-router.get('/userrecipes', async (req, res) => {
-    // grab recipes from db based on user ID 
+router.get('/userrecipes', async (req, res) => { 
+
+    // grab recipes from db based on user ID
+    await Recipe.findAll(req.user_id);
 
     //log it:
-    console.info(`${req.method} request received`);    
+    console.info(`${req.method} request received to get user's recipes`);    
 });
 
 // Route to get all recipes (or one route to get recipes based on different criteria)
 router.get('/recipes', async (req, res) => {
     // grab ALL recipes based on search criteria or specified criteria
 
+    await Recipe.findAll();
+    //ensure front end displays first 30 or whatever number
+
     //log it:
-    console.info(`${req.method} request received`);
+    console.info(`${req.method} request received to get all recipes`);
 });
 
 // Route to put edit user's recipe
 router.put('/editrecipe', async (req, res) => {
     // put to specified recipe ID applicable changes with form
+    Recipe.update(
+      {
+        // probably for loop to loop through all items from the body of the request
+      },
+      { 
+        where: {
+          recipe_id: req.recipe_id
+        }
+      }
+    )
     
     //log it:
     console.info(`${req.method} request received`);
@@ -90,14 +105,26 @@ router.put('/editrecipe', async (req, res) => {
 // Route to post comment on other users' recipes 
 router.post('/comment', async (req, res) => {
     // post to recipe comment with username of author
+// 03/13 BUILDING COMMENTS LATER AS STRETCH GOAL
+    //log it:
+    console.info(`${req.method} request received`);
 });
 
 // Route to put upvote
 router.put('/upvote', async (req, res) => {
     //put to the upvote count +1 AND prevent user from re-upvoting
+
+    //log it:
+    console.info(`${req.method} request received`);
 });
 
 // Route to put downvote
 router.put('/downvote', async (req, res) => {
     //put to the downvote count +1 AND prevent user from re-downvoting
+
+    //log it:
+    console.info(`${req.method} request received`);
 });
+
+
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,1 +1,33 @@
 // insert code here lmao
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
