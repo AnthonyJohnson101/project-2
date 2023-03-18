21ba9088dcd1ed97c33d41f52d86a94d0e433bcf
benjammo <118003873+benjammo@users.noreply.github.com>
2023-03-18 15:42:00 -0400

    added photo url column to schema and model

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -237,11 +237,8 @@ router.put('/downvote', async (req, res) => {
         }
       }
     );
-
     res.status(200); 
-
   } else { 
-
     res.status(400); 
   }
 
--- models/recipe.js
+++ models/recipe.js
@@ -26,6 +26,9 @@ Recipe.init(
     category: {
       type: DataTypes.STRING,
     },
+    recipehoto: {
+      type: DataTypes.STRING,
+    },
     ingredients: {
       type: DataTypes.TEXT,
     },
